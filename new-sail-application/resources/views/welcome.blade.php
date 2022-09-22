48107b4749aba4abb402a892b66f5985ddb416c6
lizzyb <liz@durableprogramming.com>
2022-09-22 16:12:06 -0400

    changes

--- new-sail-application/resources/views/welcome.blade.php
+++ new-sail-application/resources/views/welcome.blade.php
@@ -1,13 +1,3 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
 
         <!-- Styles -->
         <style>
@@ -19,8 +9,7 @@
                 font-family: 'Nunito', sans-serif;
             }
         </style>
-    </head>
-    <body class="antialiased">
+   
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
             @if (Route::has('login'))
                 <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
@@ -128,5 +117,3 @@
                 </div>
             </div>
         </div>
-    </body>
-</html>
