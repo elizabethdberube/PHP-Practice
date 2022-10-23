a431dd10f54728a8e29711a6ab3509109e915cc8
Lizeroni <elizabethdberube@gmail.com>
2022-10-23 18:10:45 -0400

    added register tab

--- new-sail-application/resources/views/home.blade.php
+++ new-sail-application/resources/views/home.blade.php
@@ -1,23 +1,39 @@
 @extends('layouts.app')
 
 @section('content')
+
+
+
 <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
-  
+
   <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
-    
 
 
+
+
+  <nav class="nav flex-column">
+
+  <ul class="nav nav-tabs" id="myTab" role="tablist">
+      <li class="nav-item" role="presentation">
+          <button class="nav-link active" id="home-tab" data-bs-toggle="tab" data-bs-target="#home-tab-pane" type="button" role="tab" aria-controls="home-tab-pane" aria-selected="true">Login</button>
+      </li>
+      <li class="nav-item" role="presentation">
+          <button class="nav-link" id="profile-tab" data-bs-toggle="tab" data-bs-target="#profile-tab-pane" type="button" role="tab" aria-controls="profile-tab-pane" aria-selected="false">Register</button>
+      </li>
+
+      </ul>
       <div class="tab-content" id="myTabContent">
-     <div class="card-body">
+      <div class="tab-pane fade show active" id="home-tab-pane" role="tabpanel" aria-labelledby="home-tab" tabindex="0">   
+        <div class="col-md-12">
       <div class="card row">
-     
-      <div class="card-body flex">
-     
+
+      <div class="card-body flex mt-5">
+
               <!-- Logo here -->
               <g clip-path="url(#clip0)" fill="#EF3B2D">
-              <img src="{{ asset('images/book.png') }}" alt="a really good book" width="200" height="300">
+              <img src="{{ asset('images/book.png') }}" alt="a really good book" width="200" height="200">
               </g>
-        
+
       <form method="POST" action="{{ route('login') }}">
               @csrf
 
@@ -79,11 +95,98 @@
   </div>
       </div>
   </div>
-     
+      <div class="tab-pane fade" id="profile-tab-pane" role="tabpanel" aria-labelledby="profile-tab" tabindex="0">   
+        <div class="col-md-12">
+
+      <div class="card row">
+
+
+      <div class="card-body flex mt-5">
+
+              <!-- Logo here -->
+              <g clip-path="url(#clip0)" fill="#EF3B2D">
+              <img src="{{ asset('images/book.png') }}" alt="a really good book" width="200" height="200">
+              </g>
+              <form method="POST" action="{{ route('register') }}">
+                        @csrf
+<!-- 
+                        <div class="row mb-3">
+                            <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('Name') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+
+                                @error('name')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div> -->
+
+                        <div class="row mb-3">
+                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+
+                                @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="row mb-3">
+                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+
+                                @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="row mb-3">
+                            <label for="password-confirm" class="col-md-4 col-form-label text-md-end">{{ __('Confirm Password') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                            </div>
+                        </div>
+
+                        <div class="row mb-0">
+                            <div class="col-md-6 offset-md-4">
+                                <button type="submit" class="btn btn-primary">
+                                    {{ __('Register') }}
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+          </div>
+  </div>
+
+      </div>
   </div>
 
   </div>
- 
+</nav>
+
+
+          </div>
+      </div>
+
+
+  </div>
+</div>
 
 
 @endsection
+ 
+
+
